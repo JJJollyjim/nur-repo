88da9974a875f06f201ad397659e30da6a367cf8
Jamie McClymont <jamie@kwiius.com>
2020-05-21 03:55:02 +1200

    exploitdb: init

--- default.nix
+++ default.nix
@@ -14,7 +14,8 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  exploitdb = pkgs.callPackage ./pkgs/exploitdb { };
+  exploitdbFull = pkgs.callPackage ./pkgs/exploitdb { withPapers = true; withBinaries = true; };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
